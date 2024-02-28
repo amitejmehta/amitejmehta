0f88171848aa4822aa302d54cd632e5d535358e0
Amitej Mehta <amitej@Amitejs-MacBook-Pro.local>
2024-02-28 14:38:33 -0800

    notebook-py-connection

--- utils.py
+++ utils.py
@@ -11,9 +11,11 @@ def write_cells_to_py(notebook_path: str, output_file: str, marker: str = "# WRI
     :param marker: The marker or keyword to look for in cell content.
     """
     with open(notebook_path, 'r', encoding='utf-8') as f:
+        print(f'Opened {notebook_path}')
         nb = nbformat.read(f, as_version=4)
 
     with open(output_file, 'w', encoding='utf-8') as f:
+        count = 0
         for cell in nb.cells:
             if cell.cell_type == 'code':
                 # Check if the marker is in the first line of the cell
@@ -21,8 +23,12 @@ def write_cells_to_py(notebook_path: str, output_file: str, marker: str = "# WRI
                 if lines and lines[0].strip().startswith(marker):
                     # Write the cell content without the marker
                     f.write('\n'.join(lines[1:]) + '\n\n')
+                    count += 1
+        print(f"Wrote {count} cells to {output_file}")
 
 
-if __name__ == "main":
-    write_cells_to_py('02_Original_Transformer.ipynb',
-                      '02_Transformer.py', marker="# WRITE_TO_PY")
+if __name__ == "__main__":
+    write_cells_to_py('implementing-transformers/02_Original_Transformer.ipynb',
+                      'implementing-transformers/02_Transformer.py', marker="# WRITE_TO_PY")
+    write_cells_to_py('fine-tuning/Take_Home_Coding_Task.ipynb',
+                      'fine-tuning/trainer.py', marker="#trainer.py")
